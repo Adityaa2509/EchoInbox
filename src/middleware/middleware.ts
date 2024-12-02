618ed25fa43c46fa4325d66443268f6ae3ffeed0
Aditya <geekysingh2502@gmail.com>
2024-12-02 23:39:08 +0530

    Resolved bugs regarding compatibility of Next-Auth

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -6,6 +6,8 @@ import type { NextRequest } from 'next/server'
 export async function middleware(request: NextRequest) {
     const token = await getToken({req:request});
     const url = request.nextUrl
+    
+    
     if(token && (
         url.pathname.startsWith('/sign-in') ||
         url.pathname.startsWith('/sign-up') ||
@@ -14,7 +16,12 @@ export async function middleware(request: NextRequest) {
     )){
         return NextResponse.redirect(new URL('/dashboard', request.url))
     }
-  return NextResponse.redirect(new URL('/home', request.url))
+
+    
+    if( !token && (url.pathname.startsWith('/dashboard')))
+        return NextResponse.redirect(new URL('/sign-in', request.url))
+  
+    return NextResponse.next()
 }
  
 // See "Matching Paths" below to learn more
